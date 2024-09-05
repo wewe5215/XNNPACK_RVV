64e0f856f7fc1500fc9d3fd6094492e938571463
wewe5215 <wewe5215@gmail.com>
2024-09-05 11:22:39 +0800

    feat:f32-dwconv-3p8c-minmax-rvv

--- test/f32-dwconv-minmax-unipass.cc
+++ test/f32-dwconv-minmax-unipass.cc
@@ -3596,6 +3596,24 @@ std::vector<DWConvTestParams> CreateTests2(
 #endif  // XNN_ARCH_WASM || XNN_ARCH_WASMSIMD || XNN_ARCH_WASMRELAXEDSIMD
 
 
+#if XNN_ENABLE_RISCV_VECTOR && XNN_ARCH_RISCV
+  INSTANTIATE_TEST_SUITE_P(
+      F32_DWCONV_MINMAX_3P8C__RVV, DWConvTest,
+      testing::ValuesIn(CreateTests1(
+          /*c_block=*/8, /*adj_c_block=*/8, /*cr=*/8, /*kr=*/3,
+          [](DWConvMicrokernelTester& tester) {
+            tester.Test(xnn_f32_dwconv_minmax_ukernel_3p8c__rvv,
+                        xnn_init_f32_minmax_scalar_params);
+          },
+          []() {
+            TEST_REQUIRES_RISCV_VECTOR;
+          })),
+      [](const testing::TestParamInfo<DWConvTest::ParamType>& info) {
+        return info.param.test_name;
+      });
+#endif  // XNN_ENABLE_RISCV_VECTOR && XNN_ARCH_RISCV
+
+
 INSTANTIATE_TEST_SUITE_P(
     F32_DWCONV_MINMAX_3P1C__SCALAR, DWConvTest,
     testing::ValuesIn(CreateTests1(
